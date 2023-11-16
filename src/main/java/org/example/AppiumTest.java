169984ee30417ba599fe3a12615081237594b799
hamzayaseen15 <hamzayaseen993@gmail.com>
2023-11-16 21:17:46 +0500

    architecture completed

--- src/main/java/org/example/AppiumTest.java
+++ src/main/java/org/example/AppiumTest.java
@@ -9,13 +9,15 @@ import java.net.URL;
 
 public class AppiumTest {
 
-    public static AppiumDriver getDriver() throws MalformedURLException {
+    public static AppiumDriver getDriver(String platformVersion, String udid) throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         // Set your desired capabilities here
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("automationName", "UiAutomator2");
         capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("deviceName", udid);
+        capabilities.setCapability("platformVersion", platformVersion);
 
         return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,66 @@
 package org.example;
-import io.appium.java_client.AppiumDriver;
 
+import io.appium.java_client.AppiumDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(Parameterized.class)
 public class Main {
 
     private AppiumDriver driver;
+    private final String platformVersion;
+    private final String udid;
+    private final TestScenario testScenario;
+
+    public enum TestScenario {
+        INITIATE_CALL,
+        RECEIVE_CALL
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        // Provide parameters for each device and test scenario
+        return Arrays.asList(new Object[][] {
+                {"device_version_1", "device_udid_1", TestScenario.INITIATE_CALL},
+                {"device_version_2", "device_udid_2", TestScenario.RECEIVE_CALL},
+                // Add more devices and scenarios as needed
+        });
+    }
+
+    public Main(String platformVersion, String udid, TestScenario testScenario) {
+        this.platformVersion = platformVersion;
+        this.udid = udid;
+        this.testScenario = testScenario;
+    }
 
     @Before
     public void setUp() throws MalformedURLException {
-        // Initialize the driver using AppiumConfig
-        driver = AppiumTest.getDriver();
+        // Initialize the driver using AppiumTest
+        driver = AppiumTest.getDriver(platformVersion, udid);
     }
 
     @Test
     public void myTest() {
-        // Write your test logic here using Appium commands
-        // Example: driver.findElementByAccessibilityId("myElement").click();
+        switch (testScenario) {
+            case INITIATE_CALL:
+                // Implement steps to initiate a call
+                // E.g., click on the call button, select contact, etc.
+                break;
+
+            case RECEIVE_CALL:
+                // Implement steps to receive a call
+                // E.g., wait for incoming call notification, answer the call, etc.
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + testScenario);
+        }
     }
 
     @After
